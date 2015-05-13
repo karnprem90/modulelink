853d20b517c8d0746f558e296a3b6bbe2a524099
karnprem90 <karnprem@cronj.com>
2015-05-13 17:08:46 +0530

    prepeare for 1.0.0

---
+++ index.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var Joi = require('joi'),
+    Boom = require('boom'),
+    http = require('http'),
+    querystring = require('querystring'),
+    Config = require('./config/config');
+
+exports.getClassificationAttributeHost = {
+  handler: function (request, reply) {
+      return reply(Config.host.classificationAttribute);
+  }
+};
+
+exports.classificationPOST = {
+  handler: function (request, reply) {
+    var post_data = querystring.stringify(request.payload);
+    var options = {
+      host: Config.host.classificationAttribute,
+      path: request.path,
+      method: 'POST',
+      headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              'Content-Length': post_data.length
+          }
+    };
+
+    var req = http.request(options, function(res) {
+      return reply(res);
+    });
+
+    req.on('error', function(e) {
+      console.log('problem with request: ' + e.message);
+    });
+    
+    // write data to request body
+    req.write(post_data);
+    req.end();
+  }
+};
+
+
+exports.classificationGET = {
+  handler: function (request, reply) {
+    var options = {
+      host: Config.host.classificationAttribute,
+      path: request.path,
+      method: 'GET'
+    };
+
+    var req = http.request(options, function(res) {
+      return reply(res);
+    });
+
+    req.on('error', function(e) {
+      console.log('problem with request: ' + e.message);
+    });
+    
+    req.end();
+  }
+};
+
